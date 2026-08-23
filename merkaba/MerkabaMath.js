a37114e071cdb4b2c94ca4d3db53496c351b6e0d
alexeyb2 <me1@wlink.ru>
2026-08-23 15:12:39 +0300

    Update MerkabaMath.js

--- merkaba/MerkabaMath.js
+++ merkaba/MerkabaMath.js
@@ -1,5 +1,12 @@
 export class MerkabaMath {
-    constructor(h, l, origin = {x:0, y:0, z:0}, bottomShift = {x:0, z:0}, topShift = {x:0, z:0}) {
+    /**
+     * @param {number} h - высота между вершиной нижнего и его основанием (по Y)
+     * @param {number} l - радиус описанной окружности основания
+     * @param {object} origin - точка 👁️ (вершина нижнего тетраэдра) {x,y,z}
+     * @param {object} bottomShift - смещение основания нижнего от вершины {x,z}
+     * @param {object} topShift - смещение вершины верхнего от его основания {x,z}
+     */
+    constructor(h, l, origin = {x:0,y:0,z:0}, bottomShift = {x:0,z:0}, topShift = {x:0,z:0}) {
         this.h = h;
         this.l = l;
         this.origin = origin;
@@ -14,30 +21,31 @@ export class MerkabaMath {
         const sqrt3halfL = sqrt3 * halfL;
         const { x, y, z } = this.origin;
 
-        // НИЖНИЙ ТЕТРАЭДР
-        // Вершина (👁️) остаётся в origin, основание сдвигается на -bottomShift
+        // ---------- НИЖНИЙ ТЕТРАЭДР (🔺) ----------
+        // Вершина (👁️) остаётся в origin, основание сдвигается на -bottomShift (т.е. направление взгляда нижнего - это вектор от вершины к основанию)
         const bx = x - this.bottomShift.x;
         const bz = z - this.bottomShift.z;
         const bottomBaseY = y - this.h;
 
-        // ВЕРХНИЙ ТЕТРАЭДР
-        // Основание (🔴🔵🟢) остаётся симметричным вокруг origin (немного выше)
-        const topBaseY = y - this.h + 2*d;
-        // Вершина (🟪) сдвигается на +topShift
+        // ---------- ВЕРХНИЙ ТЕТРАЭДР (🔻) ----------
+        // Основание (🔴🔵🟢) строится вокруг origin (без смещения), вершина (🟪) сдвигается на +topShift
+        const topBaseY = y - this.h + 2 * d;
         const tx = x + this.topShift.x;
         const tz = z + this.topShift.z;
-        const topVertexY = y - 2*this.h + 2*d;
+        const topVertexY = y - 2 * this.h + 2 * d;
 
         return {
+            // вершина нижнего
             '👁️': { x, y, z },
+            // основание нижнего
             '🟦': { x: bx - sqrt3halfL, y: bottomBaseY, z: bz + halfL },
             '🟥': { x: bx + sqrt3halfL, y: bottomBaseY, z: bz + halfL },
             '🟩': { x: bx,             y: bottomBaseY, z: bz - this.l },
-
+            // основание верхнего
             '🔴': { x: x + sqrt3halfL, y: topBaseY, z: z - halfL },
             '🔵': { x: x - sqrt3halfL, y: topBaseY, z: z - halfL },
             '🟢': { x: x,             y: topBaseY, z: z + this.l },
-
+            // вершина верхнего
             '🟪': { x: tx, y: topVertexY, z: tz }
         };
     }
